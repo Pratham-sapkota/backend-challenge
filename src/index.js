2bae9079391a106e6fb31130728a127e99b39d13
Pratham Sapkota <prathamsapkota1837@gmail.com>
2022-06-15 16:41:26 +0545

    merged data

--- src/index.js
+++ src/index.js
@@ -6,19 +6,27 @@ const { response } = require('express')
 
 const dbRaw=fs.readFileSync('./db/db.json')
 const dbJSON=JSON.parse(dbRaw)
-// console.log(dbJSON)
 
 const dataRaw=fs.readFileSync('./db/data.json')
 const dataJSON=JSON.parse(dataRaw)
-// console.log(dataJSON)
 
+
+let newData = []
 dbJSON.forEach(element=>{
     const dbID=element.unitId.toString()
     // console.log(dbID)
     dataJSON.forEach(data=>{
         const dataID=data.elevatorInfo.Institution_Characteristics.Unitid
         if(dbID===dataID){
-           console.log({...element},{...data})
+            let mergedData = {
+                "financialAid" : element.financialAid,
+                "retentionAndGraduation": element.retentionAndGraduation,
+                "unitId": element.unitId,
+                "elevatorInfo": data.elevatorInfo,
+                "studentCharges": data.studentCharges
+            }
+            newData.push(mergedData);
         }
     })
 })
+console.log(newData)
